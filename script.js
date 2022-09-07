fcc4b55ece26df01cbc571998d6ede22acd6a913
chhotu marandi <cmarandi29@gmail.com>
2022-09-07 16:23:40 +0530

    add setTimeout function

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ counters.forEach((counter) => {
   counter.innerHTML = 0;
 
   const updateCounter = () => {
-    const dataCount = counter.getAttribute('data-target');
+    const dataCount = +counter.getAttribute('data-target');
+
+    // console.log(typeof dataCount);
+    const intialNumber = Number(counter.innerHTML);
+    const increament = dataCount / 100;
+
+    if (intialNumber < dataCount) {
+      counter.innerHTML = `${intialNumber + increament}`;
+
+      setTimeout(updateCounter, 1000);
+    }
   };
 
   updateCounter();
